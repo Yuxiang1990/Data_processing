25041e8343c5c6eb4d41179131cd3ed633c9b2cf
yuxiang <32085657+Yuxiang1990@users.noreply.github.com>
2017-11-10 14:07:48 +0800

    Update multi_processing.py

--- multi_processing.py
+++ multi_processing.py
@@ -8,6 +8,13 @@ def task(path):
   
 with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
     futures = {executor.submit(task, arg) for arg in targets}
-    for f in tqdm(concurrent.futures.as_completed(futures),total=len(targets)):
-        pass
+#     for f in tqdm(concurrent.futures.as_completed(futures),total=len(targets)):
+#         pass
+    try:
+        for f in tqdm(concurrent.futures.as_completed(futures),total=len(targets)):
+            err = f.exception()
+            if err is not None:
+               raise err
+    except KeyboardInterrupt:
+        print("stopped by hand")
   
