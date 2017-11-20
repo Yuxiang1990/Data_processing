510f24e6f6d0f448669379ae4eddc48db05080b4
yuxiang <32085657+Yuxiang1990@users.noreply.github.com>
2017-11-20 15:51:35 +0800

    Update multi_processing.py

--- multi_processing.py
+++ multi_processing.py
@@ -17,4 +17,22 @@ with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
                raise err
     except KeyboardInterrupt:
         print("stopped by hand")
-  
+#-----------------------------------------------------------------------------------------#  
+#coding=utf-8
+import multiprocessing
+
+def do(n) :
+  #获取当前线程的名字
+  name = multiprocessing.current_process().name
+  print name,'starting'
+  print "worker ", n
+  return 
+
+if __name__ == '__main__' :
+  numList = []
+  for i in xrange(5) :
+    p = multiprocessing.Process(target=do, args=(i,))
+    numList.append(p)
+    p.start()
+    p.join()
+    print "Process end."
